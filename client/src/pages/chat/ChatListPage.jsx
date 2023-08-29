fec67c08f5aabc8a9e6f397533fa8e15782b2fa7
Phway Thant Chae <ilovejazz555@gmail.com>
2023-08-28 22:52:46 -0400

    wip - chat list ui

--- client/src/pages/chat/ChatListPage.jsx
+++ client/src/pages/chat/ChatListPage.jsx
@@ -5,11 +5,12 @@ import { UserContext } from "../../UserContext";
 import Header from "../../Header";
 import AdminNav from "../../admin-pages/AdminNav";
 import Conversation from './Conversation';
+import Message from "./Message";
 
 export default function ChatListPage() {
   const { user } = useContext(UserContext);
   return (
-    <div className="flex flex-col min-h-screen px-8 py-4">
+    <div className="flex flex-col px-8 py-4">
       <Header />
       {user && (
         <>
@@ -19,25 +20,53 @@ export default function ChatListPage() {
           )}
         </>
       )}
-      {/* <div><div class="container mx-auto py-8">
-    <div class="bg-white rounded-lg shadow-md">
-        <div class="py-4 px-6 border-b border-gray-200">
-            <h1 class="text-xl font-semibold">Messenger</h1>
-        </div>
-
-    </div>
-</div></div> */}
-      <div className="p-5 bg-white rounded-lg shadow-md">
+      <div className="p-2 bg-white rounded-lg shadow-md custom-chatbox-height">
         <div className="flex">
           <div className="w-1/3 chatMenu">
             <input type="text" placeholder="Search for users" className="" />
-            <div className="chatMenuWrapper">
+            <div className="overflow-y-auto chatMenuWrapper">
+              <Conversation/>
+              <Conversation/>
               <Conversation/>
               <Conversation/>
             </div>
           </div>
           <div className="w-2/3 chatBox">
-            <div className="chatBoxWrapper">2</div>
+            <div className="relative flex flex-col justify-between p-4 chatBoxWrapper">
+                <div className="space-y-4 overflow-y-auto chatBoxTop h-100">
+                        <Message own={true}/>
+                        <Message own={false}/>
+                        <Message own={true}/>
+                        <Message own={true}/>
+                        <Message own={false}/>
+                        <Message own={true}/>
+                        <Message own={true}/>
+                        <Message own={false}/>
+                        <Message own={true}/>
+                        <Message own={true}/>
+                        <Message own={false}/>
+                        <Message own={true}/>
+                        <Message own={true}/>
+                        <Message own={false}/>
+                        <Message own={true}/>
+                        <Message own={false}/>
+                        <Message own={true}/>
+                        <Message own={true}/>
+                        <Message own={false}/>
+                        <Message own={true}/>
+                        <Message own={false}/>
+                        <Message own={true}/>
+                        <Message own={true}/>
+                        <Message own={false}/>
+                        <Message own={true}/>
+                </div>
+
+
+                <div className="flex justify-between mt-4 chatBoxBottom">
+                    <input type="text" className="flex-grow p-2 border rounded-l-lg" placeholder="Type your message..."/>
+                    <button className="px-4 ml-4 text-white rounded-lg bg-primary">Send</button>
+                </div>
+            </div>
           </div>
         </div>
       </div>
--- client/src/pages/chat/Message.jsx
+++ client/src/pages/chat/Message.jsx
@@ -1,6 +1,9 @@
-
-export default function  Message() {
-    return (
-        <div></div>
-    )
+export default function Message({own}) {
+  return (
+    <div className={`flex ${own ? 'justify-end' : 'justify-start'}`}>
+      <div className={`max-w-xs p-3 sm:text-sm text-white rounded-lg ${own ? 'bg-gray-400' : 'bg-blue-500'}`}>
+        Hey there! How can I help you today?
+      </div>
+    </div>
+  );
 }
